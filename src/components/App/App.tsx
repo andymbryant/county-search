928d13d0ef1e23d115d4528199b5527a80718d13
Andy <a.mcdermid.bryant@gmail.com>
2019-01-12 18:01:35 -0500

    fetch and format v1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	state = {
+		isLoading: false,
+		data: null,
+		levels: []
+	}
+
+	componentDidMount() {
+		this.setState({
+			isLoading: true
+		})
+		this.fetchData()
+	}
+
+	fetchData = () => {
+		fetch("https://api.myjson.com/bins/dbg52")
+			.then(res => res.json())
+			.then(
+				(data) => {
+					this.formatData(data)
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	}
+
+	formatData = (data: any[]) => {
+		//get top level
+		let topLevelData = data.filter(d => d.parent === null)
+		this.setState({
+			data,
+			topLevelData
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<header>
 					<h1>County Search</h1>
 				</header>
+				{/* {this.state.levels.map((l) => {
+					return (
+						<GroupOption />
+					)
+				})
+
+				} */}
 			</div>
 		);
 	}
