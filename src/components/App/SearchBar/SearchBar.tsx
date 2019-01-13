4a28cae5d0eb276e366bef966ce6e7a8e8c0b43e
Andy <a.mcdermid.bryant@gmail.com>
2019-01-13 13:24:48 -0500

    styling searchbar

--- src/components/App/SearchBar/SearchBar.tsx
+++ src/components/App/SearchBar/SearchBar.tsx
@@ -8,16 +8,16 @@ type MyProps = {
 class SearchBar extends Component<MyProps> {
     render() {
         const { data } = this.props;
-        console.log(data)
-
-        const style = {
-            marginLeft: '1rem'
-        }
         return (
             <div className="search-bar">
-                {data.map((item: any) => {
-                    <div style={style}>{item.name}</div>
-                })}
+                <input type="text" className="search-field" />
+                <div className="search-items">
+                    {data.map((item: any) => {
+                        return (
+                            <div className="item" key={item.id}>{item.name}</div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
