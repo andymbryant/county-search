f53f57835f726738f75b7f5543f2b174a60dbcff
Andy <a.mcdermid.bryant@gmail.com>
2019-01-13 14:09:37 -0500

    hover styling for search items

--- src/components/App/SearchBar/SearchBar.tsx
+++ src/components/App/SearchBar/SearchBar.tsx
@@ -11,15 +11,25 @@ class SearchBar extends Component<MyProps> {
     render() {
         const { data, levels } = this.props;
         let style = {} as Object;
+        let levelClass: String = '';
 
         return (
             <div className="search-bar">
                 <input type="text" className="search-field" />
                 <div className="search-items">
                     {data.map((item: any) => {
-                        style = { "margin-left": levels.indexOf(item.level) * 1.2 + "rem" }
+                        style = { "padding-left": (levels.indexOf(item.level) + .6) * 1.666667 + "rem" }
+                        if (levels.indexOf(item.level) === levels.length - 1) {
+                            levelClass = 'bot-level'
+                        } else {
+                            levelClass = 'high-level'
+                        }
                         return (
-                            <div style={style} className="item" key={item.id}>{item.name}</div>
+                            <div
+                                style={style}
+                                className={`item ${levelClass}`}
+                                key={item.id}>{item.name}
+                            </div>
                         )
                     })}
                 </div>
