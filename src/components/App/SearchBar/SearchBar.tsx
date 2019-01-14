67d6c83b49756f009a0d8486200fe2e62665b937
Andy <a.mcdermid.bryant@gmail.com>
2019-01-14 08:00:53 -0500

    searchbar documentation

--- src/components/App/SearchBar/SearchBar.tsx
+++ src/components/App/SearchBar/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import { find } from 'lodash';
 import './SearchBar.css';
 
+// Specify the type of incoming props
 type MyProps = {
     data: any,
     levels: String[]
@@ -10,27 +10,35 @@ type MyProps = {
 class SearchBar extends Component<MyProps> {
 
     state = {
+        // Boolean representation of whether the user is focused on the search field or not
         searchFocus: false,
+        // User search query
         query: ''
     }
 
+    // From input field
+    // When input field is focused, the state is updated
     onFocus(event: any) {
         this.setState({
             searchFocus: true
         })
     }
 
+    // From input field
+    // When input field is not focused, the state is updated
     onBlur(event: any) {
         this.setState({
             searchFocus: false
         })
-
     }
 
+    // From input field
+    // Captures input text from user, makes it lowercase
     handleChange(event: any) {
         this.setState({ query: event.target.value.toLowerCase() });
     }
 
+    // From cancel button, which appears when user adds text to search field
     cancel() {
         this.setState({
             query: ''
@@ -38,13 +46,20 @@ class SearchBar extends Component<MyProps> {
     }
 
     render() {
+        // Destructure props and state
         let { data, levels } = this.props;
         const { query } = this.state;
+
+        // Initialize typed variables
         let style = {} as Object;
         let levelClass: String = '';
         let filteredData: any[] = [];
 
+        // Filter function for JSON filtering on query
+        // Initialized here for proper data binding
         function filterItems(item: any) {
+            // If the item is the bottom level, check if the query matches
+            // one of its children
             if (!item.isBotLevel) {
                 for (let d of item.children) {
                     if (d.indexOf(query) !== -1) {
@@ -52,25 +67,31 @@ class SearchBar extends Component<MyProps> {
                     }
                 }
             } else {
+                // Otherwise, check if the name of the item matches the query
                 return item.name.toLowerCase().includes(query)
             }
         }
 
+        // If there the user makes a query, filter the items accordingly
+        // If not and filteredData is empty, add data to filteredData
         if (this.state.query) {
             filteredData = data.filter(filterItems)
-        }
-        if (filteredData.length === 0 && !this.state.query) {
+        } else if (filteredData.length === 0) {
             filteredData = data
         }
 
-        if (!filteredData) {
-            <div className="search-bar">
-                <input type="text" className="search-field" placeholder="No Results" />
-            </div>
-        }
+
+        // if (!filteredData) {
+        //     return (
+        //         <div className="search-bar">
+        //             <input type="text" className="search-field" placeholder="No Results" />
+        //         </div>
+        //     )
+        // }
         return (
             <div className="search-bar">
                 <div className="search-input">
+                    {/* Input field with event handlers and functions */}
                     <input
                         type="text"
                         placeholder="Please select a county"
@@ -80,18 +101,25 @@ class SearchBar extends Component<MyProps> {
                         onFocus={(e) => this.onFocus(e)}
                         onBlur={(e) => this.onBlur(e)}
                     />
+                    {/* Conditionally render arrow by adding arrow-active class, based on user focus */}
                     <div className="search-toggle">
                         {this.state.searchFocus ? <div className="arrow arrow-active"></div> : <div className="arrow"></div>}
                     </div>
                 </div>
+                {/* Conditionally render cancel button, based on existence of user input - query */}
                 {this.state.query ?
                     <div
                         className="cancel"
                         onClick={() => this.cancel()}
                     >X</div> : null}
+                {/* Conditionally show search items, based on user focus */}
                 {this.state.searchFocus ? <div className="search-items">
+                    {/* Loop through filtered data, returning a div for reach item in list */}
                     {filteredData.map((item: any) => {
+                        // Conditional indentation, based on index of item level
+                        // in the hierarchy mapping "levels", using inline styling
                         style = { "paddingLeft": (levels.indexOf(item.level) + .6) * 1.666667 + "rem" }
+                        // Assign classes for conditional CSS
                         if (item.isBotLevel) {
                             levelClass = 'bot-level'
                         } else {
