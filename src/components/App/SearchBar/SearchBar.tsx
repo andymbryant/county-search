3cb5c248f193b64329c832a4f6d3a0f62ab7b8fd
Andy <a.mcdermid.bryant@gmail.com>
2019-01-13 19:49:18 -0500

    added search toggle and cancel button

--- src/components/App/SearchBar/SearchBar.tsx
+++ src/components/App/SearchBar/SearchBar.tsx
@@ -10,7 +10,21 @@ type MyProps = {
 class SearchBar extends Component<MyProps> {
 
     state = {
+        searchFocus: false,
         query: ''
+    }
+
+    onFocus(event: any) {
+        this.setState({
+            searchFocus: true
+        })
+    }
+
+    onBlur(event: any) {
+        this.setState({
+            searchFocus: false
+        })
+
     }
 
     handleChange(event: any) {
@@ -50,11 +64,19 @@ class SearchBar extends Component<MyProps> {
         }
         return (
             <div className="search-bar">
-                <input
-                    type="text"
-                    className="search-field"
-                    onChange={(e) => this.handleChange(e)}
-                />
+                <div className="search-input">
+                    <input
+                        type="text"
+                        className="search-field"
+                        onChange={(e) => this.handleChange(e)}
+                        onFocus={(e) => this.onFocus(e)}
+                        onBlur={(e) => this.onBlur(e)}
+                    />
+                    <div className="search-toggle">
+                        {this.state.searchFocus ? <div className="arrow arrow-active"></div> : <div className="arrow"></div>}
+                    </div>
+                </div>
+                {this.state.query ? <div className="cancel">X</div> : null}
                 <div className="search-items">
                     {filteredData.map((item: any) => {
                         style = { "paddingLeft": (levels.indexOf(item.level) + .6) * 1.666667 + "rem" }
